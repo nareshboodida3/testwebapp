3c1b83091b815835215331509cf2bd6d97294a8b
nareshboodida3 <140226972+nareshboodida3@users.noreply.github.com>
2024-06-11 18:01:22 +0530

    basic changes

---
+++ src/main/java/com/nsg/TestwebappApplication.java
@@ -0,0 +1,13 @@
+package com.nsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsg/TestwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
