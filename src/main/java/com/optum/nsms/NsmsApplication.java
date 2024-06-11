81fa4fe0f22f8118a47af40142861f1ebf8aaa38
nareshboodida3 <140226972+nareshboodida3@users.noreply.github.com>
2024-06-11 18:02:36 +0530

    basic changes

--- src/main/java/com/optum/nsms/NsmsApplication.java
+++ src/main/java/com/optum/nsms/NsmsApplication.java
@@ -1,13 +1,13 @@
-package com.nsg;
+package com.optum.nsms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestwebappApplication {
+public class NsmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestwebappApplication.class, args);
+		SpringApplication.run(NsmsApplication.class, args);
 	}
 
 }
--- src/test/java/com/optum/nsms/NsmsApplicationTests.java
+++ src/test/java/com/optum/nsms/NsmsApplicationTests.java
@@ -1,13 +1,17 @@
-package com.nsg;
+package com.optum.nsms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
-class TestwebappApplicationTests {
+class NsmsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		 
 	}
 
 }
